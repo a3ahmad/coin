97dfbb99ea56203e14426d615c59f4c000226c46
Anis Ahmad <a3ahmad@gmail.com>
2021-10-21 23:10:26 -0700

    Compression working -- I believe

--- grad.py
+++ grad.py
@@ -1,4 +1,6 @@
 import argparse
+import datetime
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,53 +10,22 @@ from torch.utils.data import TensorDataset, DataLoader
 import torchvision
 
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 
-class COIN(pl.LightningModule):
-    def __init__(self, num_hidden_layers, layer_width, lr, indices, colors):
-        super().__init__()
 
-        self.register_buffer('x', indices)
-        self.register_buffer('y', colors)
-        self.lr = lr
+# from pytorch_msssim import ssim
 
-        self.layers = nn.ModuleList([
-            nn.Linear(2, layer_width),
-        ] + [
-            nn.Linear(layer_width, layer_width)
-        ] * num_hidden_layers + [
-            nn.Linear(layer_width, 3),
-        ])
+# from sam import sam
 
-    def forward(self, x):
-        x = self.layers[0](x)
-        x = torch.sin(30.0 * x)
 
-        for layer in self.layers[1:-1]:
-            x = layer(x)
-            x = torch.sin(x)
-
-        x = self.layers[-1](x)
-        x = torch.sigmoid(x)
-
-        return x
-
-    def configure_optimizers(self):
-        return optim.Adam(
-            self.parameters(),
-            lr=self.lr)
-
-    def training_step(self, train_batch, batch_idx):
-        #x, y = train_batch
-        z = self.forward(self.x)
-        loss = F.mse_loss(z, self.y)
-        self.log('train_loss', loss)
-        return loss
+OMEGA = 30.0
 
 parser = argparse.ArgumentParser(description='COIN gradient descent implementation')
 parser.add_argument("--hidden", type=int, default=13, help="Number of hidden layers")
 parser.add_argument("--width", type=int, default=49, help="Hidden layer width")
-parser.add_argument("--lr", type=float, default=2e-4, help="Learning rate")
+parser.add_argument("--lr", type=float, default=2e-3, help="Learning rate")
 parser.add_argument("--epochs", type=int, default=50000, help="Number of epochs")
+parser.add_argument("--shuffle", type=bool, default=False, help="Shuffle each batch")
 parser.add_argument('--batch_size', type=int, help='Batch size')
 parser.add_argument('--path', required=True, help='Path of image to compress')
 
@@ -62,7 +33,6 @@ args = parser.parse_args()
 
 image = torchvision.transforms.Compose([
     torchvision.transforms.ConvertImageDtype(torch.float),
-    torchvision.transforms.Normalize(0, 255.0)
 ])(torchvision.io.read_image(args.path))
 
 # Default batch_size is the entire image
@@ -75,21 +45,108 @@ indices = torch.where(image[0, ...] >= 0)
 colors = image[:, indices[0], indices[1]].transpose(0, 1)
 norm_y = (indices[0].float() - half_height) / half_height
 norm_x = (indices[1].float() - half_width) / half_width
-indices = torch.stack((norm_y, norm_x)).transpose(0, 1)
+indices = torch.stack((norm_y, norm_x)).transpose(0, 1).cuda()
 image_dataset = TensorDataset(torch.zeros(size=(1,1)), torch.zeros(size=(1,1)))
 image_dataloader = DataLoader(image_dataset, batch_size=1)
 
-model = COIN(args.hidden, args.width, args.lr, indices, colors)
+def test_compress(name = None):
+    global indices
+    global image
+
+    regen_image = torchvision.transforms.Compose([
+        torchvision.transforms.ConvertImageDtype(torch.uint8),
+    ])(model(indices).transpose(0,1).view(image.shape))
+    if name is None:
+        torchvision.io.write_png(regen_image.cpu(), args.path.replace('.png', '_compressed.png').replace('.jpg', '_compressed.png'))
+    else:
+        torchvision.io.write_png(regen_image.cpu(), args.path.replace('.png', '_compressed.png').replace('.jpg', '_compressed.png').replace('.', f'_{name}.'))
+
+
+class COIN(pl.LightningModule):
+    def __init__(self, num_hidden_layers, layer_width, lr, shuffle, indices, colors):
+        super().__init__()
+
+        self.register_buffer('x', indices)
+        self.register_buffer('y', colors)
+        self.lr = lr
+        self.shuffle = shuffle
+
+        self.layers = nn.ModuleList(self.init_weights([
+            nn.Linear(2, layer_width),
+        ], 1.0 / 2.0) + self.init_weights([
+            nn.Linear(layer_width, layer_width),
+        ] * num_hidden_layers, math.sqrt(6.0 / layer_width) / OMEGA) + self.init_weights([
+            nn.Linear(layer_width, 3),
+        ], math.sqrt(6.0 / layer_width) / OMEGA))
+
+    def init_weights(self, linear_layers, scale):
+        for layer in linear_layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.uniform_(layer.weight, -scale, scale)
+
+        return linear_layers
+
+    def forward(self, x):
+        x = self.layers[0](x)
+        x = torch.sin(OMEGA * x)
+
+        for layer in self.layers[1:-1]:
+            x = layer(x)
+            x = torch.sin(OMEGA * x)
+
+        x = self.layers[-1](x)
+        x = torch.sigmoid(x)
+
+        return x
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(
+            self.parameters(),
+            lr=self.lr)
+
+        lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=500, verbose=True)
+
+        scheduler = {
+            'scheduler': lr_scheduler,
+            'reduce_on_plateau': True,
+            # val_checkpoint_on is val_loss passed in as checkpoint_on
+            'monitor': 'train_loss'
+        }
+        return [optimizer], [scheduler]
+
+        #optimizer = sam.SAM(self.parameters(), torch.optim.SGD, lr=self.lr, momentum=0.9)
+        #return optimizer
+
+    def training_epoch_end(self, outputs):
+        if self.trainer.current_epoch % 1000 == 0:
+            test_compress(self.trainer.current_epoch)
+
+    def training_step(self, train_batch, batch_idx):
+        if self.shuffle:
+            idx = torch.randperm(self.x.shape[0])
+            z = self.forward(self.x[idx])
+            loss = F.mse_loss(z, self.y[idx])
+
+            self.log('train_loss', loss)
+            return loss
+        else:
+            z = self.forward(self.x)
+            loss = F.mse_loss(z, self.y)
+
+            self.log('train_loss', loss)
+            return loss
+
+
+model = COIN(args.hidden, args.width, args.lr, args.shuffle, indices, colors)
 
 trainer = pl.Trainer(
     gpus=1 if torch.cuda.is_available() else 0,
-    precision=16 if torch.cuda.is_available() else 32,
+    logger=WandbLogger(project="COIN", id=f"{OMEGA}-{args.hidden}-{args.width}-{args.lr}-{datetime.datetime.now().strftime('%d-%m-%y-%Ih%Mmin')}"),
+#    precision=16 if torch.cuda.is_available() else 32,
     max_epochs=args.epochs
     )
+
 trainer.fit(model, image_dataloader)
 
-image = torchvision.transforms.Compose([
-    torchvision.transforms.Normalize(0, 1.0 / 255.0),
-    torchvision.transforms.ConvertImageDtype(torch.uint8),
-])(model(indices).transpose(0,1).view(image.shape))
-torchvision.io.write_png(image, args.path.replace('.png', '_compressed.png').replace('.jpg', '_compressed.png'))
+indices = indices.cpu()
+test_compress()
